13bb8db636e0c071a218ee15ac7f472051cb89e2
Daniel Prochazka <danielprochazka@gmail.com>
2023-05-26 17:18:31 +0200

    refactoring math and design

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # React-calculator-app
-Beginner project - React calculator
+Beginner project - React scietific calculator
+
+## Calculator functions
+COS - cosinus function\
+TAN - tangens function\
+SIN - sinus function\
++- - append -/+ infront of value\
+ABS - absolute value\
--- calculator/src/index.js
+++ calculator/src/index.js
@@ -1,12 +1,10 @@
-import React from "react";
+import React from 'react';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './Worker';
 import { createRoot } from 'react-dom/client';
-import App from "./App";
-import "./index.css";
 
-const root = createRoot(document.getElementById('root'));
-
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App/>);
+registerServiceWorker();
